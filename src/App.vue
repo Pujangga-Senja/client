dadc891319effc88028b064a1fe275fdec6cdc6d
m4rk00s <marktony.x3.24@gmail.com>
2020-01-17 09:09:09 +0700

    fitur login selesai

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,71 @@
 <template>
-  <h1>{{ message }}</h1>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>{{ count }}</h1>
+    <HelloWorld v-on:add-count="addCounter" v-bind:msg="message" />-->
+    <Header v-bind:isUserLogin="isUserLogin"></Header>
+    <Login v-show="!isUserLogin" v-on:userLogin="login" v-on:userRegister="register"></Login>
+    <Content v-show="isUserLogin"></Content>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+// import HelloWorld from "./components/HelloWorld";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
 export default {
+  name: "app",
   data() {
     return {
-      message: 'Hello world',
+      isUserLogin: false, // 0 --- login page, 1 --- content page
+      message: "Hi, Markus!",
+      count: 0
     };
   },
+  components: {
+    Header,
+    Login,
+    Content,
+    Footer
+  },
+  methods: {
+    addCounter(increment, thres) {
+      // this.count = thres;
+      if (this.count > thres) {
+        this.count = 0;
+      } else {
+        this.count += increment;
+      }
+    },
+    login(data) {
+      // eslint-disable-next-line no-console
+      console.log(data);
+
+      // kalau berhasil
+      this.isUserLogin = true;
+    },
+    register(data) {
+      // eslint-disable-next-line no-console
+      console.log(data);
+
+      // kalau berhasil
+      this.isUserLogin = true;
+    }
+  }
 };
 </script>
 
-<style scoped>
+<style>
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer" style="user-select: auto;">
+    <div class="container" style="user-select: auto;">
+      <span class="text-muted" style="user-select: auto;">Pujangga Senja</span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height: 60px;
+  line-height: 60px; /* Vertically center the text there */
+  background-color: #f5f5f5;
+}
+</style>
